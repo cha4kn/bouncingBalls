5c518e18379b6a7e2172ecce773076a5f0949b12
Arvid Forsberg <aforsberg97@gmail.com>
2024-07-04 16:46:50 +0200

    1) Went over to VSCode for Python as well. 2) Frontend can now use imported protos. *) I hade to downgrade my protoc version to make it compatible with the python protobuf package.

--- frontend/client.py
+++ frontend/client.py
@@ -7,26 +7,55 @@ Created on Thu Jul  4 13:47:13 2024
 
 import zmq
 import time
+import numpy as np
 import gui
+import sys
+import bouncingBallsMessages_pb2 as ballProto
 
+# These are parameters of this world
+number_of_balls = 100
+x_max = 200
+y_max = 150
+r_max = 10
 
-ctx = zmq.Context().instance()
-socket = ctx.socket(zmq.SUB)
+x_vals = np.zeros((100,1))
+y_vals = np.zeros((100,1))
+r_vals = r_max * np.random.rand(100, 1)
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+
+print("Connecting to Bouncing Balls server...")
 socket.connect("tcp://localhost:5555")
-socket.setsockopt(zmq.SUBSCRIBE, b"UPDATE")
+print("Connected to server!")
+
+
 
 
 
 def main():
-    print(gui.plotAllBalls())
-    while True:
-        message = socket.recv()
-        print("Received message: %s", message)
-        time.sleep(2000)
-        
-        socket.send(b"Poops")
+    currentStateUpdate = ballProto.stateUpdate()
+    ball = ballProto.Ball()
+    ball.x = 20
+    ball.y = 10
+    ball.r = 5
+    ball_serialized = ball.SerializeToString()
+    print(ball_serialized)
 
+    ball2 = ballProto.Ball()
+    ball2.ParseFromString(ball_serialized)
+#    print(ball2)
+ 
+    currentStateUpdate.balls.append(ball)
+    currentStateUpdate.balls.append(ball2)
 
+    print(currentStateUpdate)
+    print(currentStateUpdate.SerializeToString())
+
+    time.sleep(2)
+
+def initializeAtRandom():
+    return    
 
 if __name__ == "__main__":
     main()
--- frontend/gui.py
+++ frontend/gui.py
@@ -5,7 +5,7 @@ Created on Mon Apr 10 10:59:47 2023
 @author: Arvid
 """
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 
 def plotAllBalls() -> str:
