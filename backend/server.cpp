8a4237f6ca4e4950f7d37e850a973d9ad759fa3e
Arvid Forsberg <aforsberg97@gmail.com>
2024-07-14 12:20:37 +0200

    1) Backend: CMake compatibility - Added CMakeLists.txt 2) Backend: Added server.cpp 3) Backend: Some change to .vscode/settings.json

---
+++ backend/server.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <time.h>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <stdio.h>
+using namespace std;
+//#include <zmq.hpp>
+
+int main() {
+    std::cout<<"Helo!"<<endl;
+    return 0;
+}
