1b4f324a944b9d8a72aedbc82543c4ded43295da
Arvid Forsberg <aforsberg97@gmail.com>
2024-07-14 21:42:15 +0200

    1) Backend: added cppzmq to CMakeLists.txt link libraries 2) Backend: added zmq usage example to server.cpp (This commit was after I moved over to WSL)

--- backend/server.cpp
+++ backend/server.cpp
@@ -5,9 +5,14 @@
 #include <thread>
 #include <stdio.h>
 using namespace std;
-//#include <zmq.hpp>
+#include <zmq.hpp>
 
 int main() {
-    std::cout<<"Helo!"<<endl;
+    std::cout<<"Hello!"<<endl;
+    zmq::context_t ctx;
+    zmq::socket_t sock(ctx, zmq::socket_type::push);
+    sock.bind("inproc://test");
+    sock.send(zmq::str_buffer("Hello, world"), zmq::send_flags::dontwait);
+    std::cout<<"ZeroMq stuff worked!"<<endl;
     return 0;
 }
