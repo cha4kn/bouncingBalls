a2abba802ecd29d5c7d8805a37b53d876051a747
Arvid Forsberg <aforsberg97@gmail.com>
2024-07-15 11:13:58 +0200

    1) Backend: successfully linked ballProto into server. 2) Backend: added some code to server to test that ballProto works. 3) Proto: added package line.

--- backend/server.cpp
+++ backend/server.cpp
@@ -4,11 +4,29 @@
 #include <chrono>
 #include <thread>
 #include <stdio.h>
-using namespace std;
 #include <zmq.hpp>
+#include "bouncingBallsMessages.pb.h"
+using namespace std;
 
 int main() {
-    std::cout<<"Hello!"<<endl;
+    std::cout<<"Verifying protobuf version..."<<endl;
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    std::cout<<"Protobuf version OK!"<<endl;
+
+    std::cout<<"Adding balls to initial state..."<<endl;
+    ballProto::stateUpdate initialState;    
+    // Add balls to the state
+    for (int i = 0; i < 10; i++) {
+        ballProto::Ball* ballToAdd = initialState.add_balls();
+        ballToAdd->set_id(i+1);
+        ballToAdd->set_r(5);
+        ballToAdd->set_x(i+1);
+        ballToAdd->set_y(10-i);
+    }
+    std::cout<<"Finished. Here is the initial state: "<<endl;
+    std::cout<<initialState.DebugString()<<endl;
+
+    std::cout<<"Testing ZeroMQ stuff..."<<endl;
     zmq::context_t ctx;
     zmq::socket_t sock(ctx, zmq::socket_type::push);
     sock.bind("inproc://test");
